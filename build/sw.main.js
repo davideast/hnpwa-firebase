5c2d8bcc0e5b00d22fad548e29183767713bba12
David East <david@firebase.com>
2017-07-03 15:06:43 -0700

    feat(ts): Adding ts for build

--- build/sw.main.js
+++ build/sw.main.js
@@ -7,5 +7,5 @@ self.addEventListener('activate', event => event.waitUntil(self.clients.claim())
 
 workboxSW.precache([]);
 
-workboxSW.router.registerRoute('/', workboxSW.strategies.networkFirst());
-workboxSW.router.registerRoute(/^\/$|news|newest|show|ask|jobs/, workboxSW.strategies.networkFirst());
+workboxSW.router.registerRoute('/', workboxSW.strategies.staleWhileRevalidate());
+workboxSW.router.registerRoute(/^\/$|news|newest|show|ask|jobs|item/, workboxSW.strategies.staleWhileRevalidate());
