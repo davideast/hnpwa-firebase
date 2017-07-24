4aee8ad1258e807ac91d4b14cf707245191c15e5
David East <deast@google.com>
2017-07-24 16:15:40 -0600

    feat(offline): offline serving. match deployed folder names to src names.

--- src/build/offline-server.ts
+++ src/build/offline-server.ts
@@ -5,4 +5,5 @@ const app = express();
 app.use(router);
 
 const PORT = process.env['PORT'] || 3004;
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+const API_BASE = process.env['API_BASE'] || 'http://localhost:3002';
+app.listen(PORT, () => console.log(`Listening on ${PORT}. Using API: ${API_BASE}`));
